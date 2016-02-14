533669572282105be4ff2871271229454edbbcd6
Tahia Khan <tahia.khan@gmail.com>
2016-02-13 20:53:38 -0500

    adding initialize_ctx method

--- ECE568-lab2/server.c
+++ ECE568-lab2/server.c
@@ -8,6 +8,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include <openssl/ssl.h>
+
 #define PORT 8765
 
 /* use these strings to tell the marker what is happening */
@@ -15,6 +17,51 @@
 #define FMT_CLIENT_INFO "ECE568-SERVER: %s %s\n"
 #define FMT_OUTPUT "ECE568-SERVER: %s %s\n"
 #define FMT_INCOMPLETE_CLOSE "ECE568-SERVER: Incomplete shutdown\n"
+
+#define SERVER_KEY      "bob.pem"
+#define SERVER_CERT     "bob.pem"
+#define CA_CERT         "568ca.pem"
+
+int pem_passwd_cb(char *buf, int size, int rwflag, void *password)
+{
+    strncpy(buf, (char *)(password), size);
+    buf[size - 1] = '\0';
+    return(strlen(buf));
+}
+
+SSL_CTX *initialize_ctx (char *keyfile, char *password)
+{
+    SSL_METHOD *meth;
+    SSL_CTX *ctx;
+
+    /* Global system initialization */
+    SSL_library_init();
+    SSL_load_error_strings();
+    
+    /* Set up a SIGPIP handler */
+
+    /* Create server context with SSLv23 method, which can accept hello msgs
+     * from SSLv2, SSLv3 and TLSv1 */
+    meth = SSLv23_method(); 
+    ctx = SSL_CTX_new(meth);
+    
+    /* Load cert */
+    if (!(SSL_CTX_use_certificate_chain_file(ctx, keyfile)))
+	    fprintf(stderr,"can't read certificate file");
+
+    /* Set private key */
+    SSL_set_default_passwd_cb(ctx, pem_passwd_cb);
+    SSL_CTX_set_default_passwd_cb_userdata(ctx, (void *) password);
+    if (!(SSL_CTX_use_PrivateKey_file(ctx, keyfile, SSL_FILETYPE_PEM)))
+	    fprintf(stderr,"can't read key file");
+
+    /* Load CA */
+    if (!(SSL_CTX_load_verify_locations(ctx, CA_CERT, 0)))
+	    fprintf(stderr,"can't read CA list");
+
+    return ctx;
+}
+
 
 int main(int argc, char **argv)
 {
