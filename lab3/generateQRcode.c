d8df16e098aed11f247ca1d60d378dad9d500b2b
Tahia  Khan <tahia.khan@gmail.com>
2016-03-14 19:46:38 -0400

    adding padding logic for secret

--- lab3/generateQRcode.c
+++ lab3/generateQRcode.c
@@ -5,6 +5,7 @@
 
 #include "lib/encoding.h"
 
+
 int
 main(int argc, char * argv[])
 {
@@ -16,16 +17,27 @@ main(int argc, char * argv[])
 	char *	issuer = argv[1];
 	char *	accountName = argv[2];
 	char *	secret_hex = argv[3];
+	char 	padded_secret_hex[20];
+	int 	i, prelen, len;
 
-	assert (strlen(secret_hex) <= 20);
+	len = strlen(secret_hex);
+	assert (len <= 20);
 
+	prelen = 20 - len;
+	for (i = 0; i < prelen; i++)
+		padded_secret_hex[i] = '0';
+	for ( ; i < 20; i++) 
+		padded_secret_hex[i] = secret_hex[i - prelen];
+  	padded_secret_hex[i] = '\0';
+	
 	printf("\nIssuer: %s\nAccount Name: %s\nSecret (Hex): %s\n\n",
-		issuer, accountName, secret_hex);
+		issuer, accountName, padded_secret_hex);
 
 	// Create an otpauth:// URI and display a QR code that's compatible
 	// with Google Authenticator
 
-	displayQRcode("otpauth://testing");
+	displayQRcode("otpauth://totp/gibson?issuer=ECE568&secret=CI2FM6EQCI2FM6EQ&period=30");
 
 	return (0);
 }
+
